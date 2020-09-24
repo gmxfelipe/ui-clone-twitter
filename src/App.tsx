075081564436fe5eb69b32cd9b3bd4d67968bbc8
Felipe Augusto Gomes de Oliveira <fagoliveira@fiecalunos.lan>
2020-09-24 17:16:45 -0300

    Inicializando ProfilePage com TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlobalStyles  from '../src/styles/GlobalStyles';
+import Layout from '../src/components/Layout';
+
+function App() {
+  return (
+    <div className="App">
+      <Layout />
+      <GlobalStyles />
+    </div>
+  );
+}
+
+export default App;
